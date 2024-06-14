2342bab07081322ed93a82b5b848fd77e870175e
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-14 15:09:59 +0600

    refactor App; added two containers to App;

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
 import './App.css';
-import AddFilmForm from './components/AddFilmForm/AddFilmForm';
-import React, {useState} from 'react';
-import {FilmName} from './types';
-import FilmField from './components/FilmField/FilmField';
+import JokeBox from './containers/JokeBox/JokeBox';
+import MovieApp from './containers/MovieApp/MovieApp';
 
 const App = () => {
-  const [filmTitles, setFilmsTitles] = useState<FilmName[]>([]);
-
-  const addFilm = (newFilm: FilmName) => {
-    setFilmsTitles((prevFilms) => [...prevFilms, newFilm]);
-  };
-
-  const deleteFilm = (id: string) => {
-    setFilmsTitles((prevFilm) => {
-      return prevFilm.filter((film) => film.id !== id);
-    });
-  };
-
-  const changeFilmName = (event: React.ChangeEvent<HTMLInputElement>, id: string) => {
-    setFilmsTitles((prevState) => {
-      return prevState.map((film) => {
-        if (film.id === id) {
-          return {...film, filmTitle: event.target.value};
-        }
-        return film;
-      });
-    });
-  };
-
 
   return (
     <>
-      <AddFilmForm onSubmit={addFilm}/>
-      <div className="container mt-4">
-        {filmTitles.length > 0 && <h5>To watch list:</h5>}
-        {filmTitles.map((film: FilmName) => {
-          return <FilmField
-            key={film.id}
-            title={film.filmTitle}
-            onRemove={() => deleteFilm(film.id)}
-            onChangeTitle={(event) => changeFilmName(event, film.id)}/>;
-        })}
+      <div className="container d-flex gap-2 mt-2">
+        <div className="col-7 border p-3">
+          <MovieApp/>
+        </div>
+        <div className="col-5 border p-3">
+          <JokeBox/>
+        </div>
       </div>
     </>
   );
